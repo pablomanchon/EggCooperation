db11b16664e9b82eb7d18daa911ed8818336373b
Pablo Manchon <pablomanchon97@gmail.com>
2023-05-18 00:28:18 -0300

    Agrego botones y logro mostrar y crear Editoriales

--- Spring/EjercicioEgg/src/main/java/com/Biblioteca/EjercicioEgg/controladores/EditorialControlador.java
+++ Spring/EjercicioEgg/src/main/java/com/Biblioteca/EjercicioEgg/controladores/EditorialControlador.java
@@ -1,13 +1,17 @@
 package com.Biblioteca.EjercicioEgg.controladores;
 
+import com.Biblioteca.EjercicioEgg.entidades.Editorial;
 import com.Biblioteca.EjercicioEgg.excepciones.MiExcepcion;
 import com.Biblioteca.EjercicioEgg.servicios.ServicioEditorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/editorial")
@@ -19,12 +23,21 @@ public class EditorialControlador {
         return "editorial_form.html";
     }
     @PostMapping("/registro")
-    public String registro(@RequestParam("nombre") String nombre) {
+    public String registro(@RequestParam(required = false) String nombre, ModelMap modelo) {
         try {
             editorialServ.crearEditorial(nombre);
+            modelo.put("exito", "Editorial creada con éxito!");
         } catch (MiExcepcion e) {
-            System.out.println(e.getMessage());
+            modelo.put("error", e.getMessage());
+            return "editorial_form.html";
         }
         return "index.html";
+    }
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo) {
+        List<Editorial> editoriales = editorialServ.listarEditoriales();
+        System.out.println(editoriales.get(0).getNombre());
+        modelo.addAttribute("editoriales",editoriales);
+        return "editorial_lista.html";
     }
 }
--- Spring/EjercicioEgg/src/main/java/com/Biblioteca/EjercicioEgg/controladores/LibroControlador.java
+++ Spring/EjercicioEgg/src/main/java/com/Biblioteca/EjercicioEgg/controladores/LibroControlador.java
@@ -1,5 +1,6 @@
 package com.Biblioteca.EjercicioEgg.controladores;
 
+import com.Biblioteca.EjercicioEgg.entidades.Libro;
 import com.Biblioteca.EjercicioEgg.excepciones.MiExcepcion;
 import com.Biblioteca.EjercicioEgg.servicios.ServicioAutor;
 import com.Biblioteca.EjercicioEgg.servicios.ServicioEditorial;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,21 +27,30 @@ public class LibroControlador {
     ServicioAutor autorServ;
     @Autowired
     ServicioEditorial editorialServ;
+
     @GetMapping("/registrar")
     public String registrar() {
         return "libro_form.html";
     }
+
     @PostMapping("/registro")
-    public String registro(@RequestParam Long isbn, @RequestParam String titulo, @RequestParam Integer ejemplares,@RequestParam Double precio,
-                           @RequestParam String idAutor, @RequestParam String idEditorial, ModelMap modelo){
+    public String registro(@RequestParam(required = false) Long isbn, @RequestParam(required = false) String titulo, @RequestParam(required = false) Integer ejemplares, @RequestParam(required = false) Double precio,
+                           @RequestParam(required = false) String idAutor, @RequestParam(required = false) String idEditorial, ModelMap modelo) {
         try {
-            libroServ.crearLibro(isbn,titulo,precio,ejemplares,idEditorial,idAutor);
+            libroServ.crearLibro(isbn, titulo, precio, ejemplares, idEditorial, idAutor);
             modelo.put("exito", "El libro fue creado correctamente!");
         } catch (MiExcepcion e) {
+            System.out.println("Error: " + e.getMessage());
             modelo.put("error", e.getMessage());
             Logger.getLogger(LibroControlador.class.getName()).log(Level.SEVERE, null, e);
             return "libro_form.html";
         }
         return "index.html";
+    }
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo){
+        List<Libro> libros = libroServ.listarLibros();
+        modelo.addAttribute("libros", libros);
+        return "libro_lista.html";
     }
 }
